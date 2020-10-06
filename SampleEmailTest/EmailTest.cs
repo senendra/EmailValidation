571cbebe0a9684545b6de98ccd9a66574f80f475
Senendra <senendra.007@gmail.com>
2020-10-06 22:26:02 +0530

    [Senendra] Add: Testing email is valid or not

---
+++ SampleEmailTest/EmailTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SampleEmailTest
+{
+    class EmailTest
+    {
+        public string regexEmail = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]+)*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        public bool RegexEmailTest(string email)
+        {
+            return Regex.IsMatch(email, regexEmail);
+        }
+    }
+}
---
+++ SampleEmailTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SampleEmailTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter email: ");
+            string email = Console.ReadLine();
+            EmailTest test = new EmailTest();
+            bool result = test.RegexEmailTest(email);
+            Console.WriteLine(email+"  "+result);
+        }
+    }
+}
